ArrowOS-Devices/android_device_xiaomi_sweet

sweet: devicesettings: introduce color profiles for KCAL
Change-Id: Ia223f80ad67b890bb0d511773f94778fabaa3d2c

devicesettings/src/org/lineageos/settings/device/utils/KcalUtils.java
@@ -27,6 +27,14 @@ public final class KcalUtils {
     public static final String KCAL_SATURATION_NODE = "/sys/devices/platform/kcal_ctrl.0/kcal_sat";
     public static final String KCAL_CONTRAST_NODE = "/sys/devices/platform/kcal_ctrl.0/kcal_cont";
 
+    private static final String[] PROFILE_SETTINGS = {"red", "green", "blue", "saturation", "contrast"};
+
+    private static final int[][] COLOR_PROFILES_VALUES = {
+        {256, 256, 256, 255, 255}, // Default
+        {240, 240, 256, -1, -1}, // Cold
+        {256, 256, 240, -1, -1},
+        {-1, -1, -1, 265, -1}}; // Saturated
+
     // Write the given value to the given position on the KCAL node
     public static void writeConfigToNode(String node, int position, int value) {
         String mDefaultRgbFormat = "R G B";
@@ -94,6 +102,25 @@ public final class KcalUtils {
                  return 224;
              default:
                  return 1;
+        }
+    }
+
+    public static void setColorProfile(int profileIndex, SharedPreferences sharedPrefs) {
+        try {
+            int[] profileSettings = COLOR_PROFILES_VALUES[profileIndex];
+            if (profileSettings.length != PROFILE_SETTINGS.length) {
+                return;
+            }
+
+            for (int i = 0; i < profileSettings.length; i++) {
+                if (profileSettings[i] != -1) {
+                    sharedPrefs.edit().putInt(PROFILE_SETTINGS[i] + "_slider", profileSettings[i]).apply();
+                }
+            }
+
+            writeCurrentSettings(sharedPrefs);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
         }
     }
 }

thefaizanbashir/S_device_xiaomi_sweet

devicesettings/src/org/lineageos/settings/device/utils/KcalUtils.java
@@ -27,6 +27,14 @@ public final class KcalUtils {
     public static final String KCAL_SATURATION_NODE = "/sys/devices/platform/kcal_ctrl.0/kcal_sat";
     public static final String KCAL_CONTRAST_NODE = "/sys/devices/platform/kcal_ctrl.0/kcal_cont";
 
+    private static final String[] COLOR_PROFILE_SETTINGS = {"red", "green", "blue", "saturation", "contrast"};
+
+    private static final int[][] COLOR_PROFILES_VALUES = {
+        {256, 256, 256, 255, 255}, // Default
+        {240, 240, 256, -1, -1}, // Cold
+        {256, 256, 240, -1, -1}, // Warm
+        {-1, -1, -1, 265, -1}}; // Saturated
+
     // Write the given value to the given position on the KCAL node
     public static void writeConfigToNode(String node, int position, int value) {
         String mDefaultRgbFormat = "R G B";
@@ -94,6 +102,25 @@ public final class KcalUtils {
                  return 224;
              default:
                  return 1;
+        }
+    }
+
+    public static void setColorProfile(int profileIndex, SharedPreferences sharedPrefs) {
+        try {
+            int[] profileSettings = COLOR_PROFILES_VALUES[profileIndex];
+            if (profileSettings.length != COLOR_PROFILE_SETTINGS.length) {
+                return;
+            }
+
+            for (int i = 0; i < profileSettings.length; i++) {
+                if (profileSettings[i] != -1) {
+                    sharedPrefs.edit().putInt(COLOR_PROFILE_SETTINGS[i] + "_slider", profileSettings[i]).apply();
+                }
+            }
+
+            writeCurrentSettings(sharedPrefs);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
         }
     }
 }